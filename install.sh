3ce0c55e443b81a1b1787eaacdbe4ba851cccc94
João Chapa <reischapa@gmail.com>
2019-10-06 18:58:10 +0100

    feat: initial working version

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+echo CURRENT_DIR=`pwd`;
 
-echo "Setting git config..."
-git config --global user.name "João Chapa"
-git config --global user.email "reischapa@gmail.com"
+cd ~
 
 echo "Installing packages..."
-sudo pacman -S \
+sudo pacman --noconfirm -S \
   git \
   autojump \
   nodejs \
@@ -37,59 +33,90 @@ sudo pacman -S \
   ufw \
   jdk-openjdk
 
-echo "enabling ufw..."
+# vim and gvim are in conflict
+pacman -R --noconfirm vim
+pacman --noconfirm gvim
+
+if [ -z `command -v yay` ]; then
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+fi
+
+cd;
+
+function yeet() {
+  yay -Sy --noconfirm --answerclean 'N' --answerdiff 'N' --answeredit 'N' "$@"
+}
+
+yeet jq
+yeet feh
+yeet i3-wm
+yeet rofi
+yeet polybar
+yeet compton
+yeet redshift
+yeet ttf-ms-fonts
+yeet ttf-liberation
+yeet ttf-symbola
+yeet noto-fonts-sc
+yeet noto-fonts-tc
+yeet otf-ipafont
+yeet ttf-iosevka
+yeet ephifonts
+yeet tlp
+yeet xscreensaver
+
+if [ ! -z `command -v n` ]; then
+  echo 'Setting up node/npm/n...'
+  sudo npm install --global n
+
+  n 10
+
+  npm install --global nodemon
+  npm install --global http-server
+  npm install --global lerna
+fi
+
+echo 'Cloning dot-files repo'
+
+cd ~
+
+rm .bashrc;
+rm .bash_logout;
+
+eval $(ssh-agent) && ssh-add
+
+git init
+git remote add origin https://github.com/reischapa/dot-files.git
+git pull origin master
+git submodule update --init --recursive
+
+echo "Setting git config..."
+touch .gitignore_global
+
+git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name "João Chapa"
+git config --global user.email "reischapa@gmail.com"
+
+echo "Enabling ufw..."
 sudo ufw enable
 
-echo "jq"
-yay jq
+if [ ! -f /etc/systemd/system/lock-on-suspend.service ]; then
+  echo "Installing lock on suspend service..."
 
-echo "feh" 
-yay feh
+  cd "$CURRENT_DIR"
+  
+  sudo cp -v lock-on-suspend.service /etc/systemd/system/
+  sudo systemctl daemon-reload
+  sudo systemctl enable lock-on-suspend.service
+  sudo systemctl start lock-on-suspend.service
 
-echo "i3"
-yay i3-wm
+  cd ~
+fi
 
-echo "rofi" 
-yay rofi
+echo "Enabling suspend on lid down..."
+if [ -z `cat /etc/systemd/logind.conf | grep -e '^HandleLidSwitch=suspend'` ]; then
+  sudo sed -i 's/^#HandleLidSwitch=suspend/HandleLidSwitch=suspend/g' /etc/systemd/logind.conf
+fi
 
-echo "polybar"
-yay polybar
-
-echo "compton" 
-yay compton
-
-echo "redshift" 
-yay redshift
-
-echo "vim (gvim)" 
-yay gvim
-
-echo "fonts" 
-yay ttf-ms-fonts
-yay ttf-liberation
-yay ttf-symbola
-yay noto-fonts-sc
-yay noto-fonts-tc
-yay otf-ipafont
-yay ttf-iosevka
-yay ephifonts
-
-echo "firefox"
-yay firefox-nightly
-
-echo "tlp" 
-yay tlp
-
-echo "xscreensaver"
-yay xscreensaver
-
-echo "intellij"
-yay intellij-idea-ultimate-edition
-
-sudo npm install --global n
-
-n 10
-
-npm install --global nodemon
-npm install --global http-server
-npm install --global lerna
